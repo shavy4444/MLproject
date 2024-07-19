5917c0d50095dbac99b932f170a3844f3fe6d86e
shavy4444 <shavy4444@gmail.com>
2024-07-19 15:18:30 +0530

    setup and req

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages,setup
+from typing import List
+
+HYPHEN_E='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''This funtion will return list of requirements'''
+
+    requirements=[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPHEN_E in requirements:
+            requirements.remove(HYPHEN_E)
+        
+    return requirements
+
+
+
+setup(
+
+name="MLproject",
+version="0.0.1",
+author="SHIVAM",
+author_email='shavy4444@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('Requirements.txt')
+
+)
